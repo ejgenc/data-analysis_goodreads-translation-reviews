04227fe3de9f14de91921222dffb9e4e823131d9
ejgenc <33980760+ejgenc@users.noreply.github.com>
2020-11-19 13:09:07 +0300

    Tidied up the folders.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A brief proof-of-concept implementation of my bachelor's paper',
+    description='A brief proof-of-concept implementation of my bachelors paper',
     author='Eren Janberk Genç',
     license='MIT',
 )
