e57fb1e878749ff19000e6233eaf75eed5cb24bd
ejgenc <33980760+ejgenc@users.noreply.github.com>
2021-03-11 22:59:55 +0300

    Working on implementing clean_modifiers_raw.py

--- src/data_cleaning/clean_modifiers_raw.py
+++ src/data_cleaning/clean_modifiers_raw.py
@@ -19,6 +19,11 @@ Reports related to the cleaning process are located at:
     ../../data/cleaning_reports/...
     
 """
+#%% --- Import required packages ---
+
+import os
+from pathlib import Path # To wrap around filepaths
+import pandas as pd
 
 #%% --- Set proper directory to assure integration with doit ---
 
@@ -30,5 +35,41 @@ os.chdir(dname)
 
 import_fp = Path("../../data/raw/modifiers_raw.csv")
 modifiers = pd.read_csv(import_fp, encoding = "utf-8")
+
+#%% --- Cleaning: filter, document and drop null values ---
+
+#Create a mask for null values
+null_values_mask = modifiers.isnull()
+
+# null_values_mask.sum().sum() != 0, so there are null values
+
+#%% --- Cleaning: assure data type agreement between and within columns ---
+
+error_list = []
+
+for column_name in modifiers.columns:
+    expected_dtype = type(modifiers[column_name][0])
+    value_index = 0
+    while value_index < len(modifiers[column_name]):
+        actual_dtype = type(modifiers[column_name][value_index])
+        if expected_dtype != actual_dtype:
+            error_entry = [column_name, value_index, expected_dtype, actual_dtype]
+            error_list.append(error_entry)
+        value_index += 1
+        
+# There appears to be no proof of data-type disagreement. Moving on.
+
+#%% --- Cleaning: drop specific modifiers that are known false positives ---
+
+#%% --- Cleaning: clean modified variation ---
+
+#DO THIS NEXT: create a detailed list of all permissible forms
+# check existing form match with permissible forms. Drop all that isn't.
+
+#modified_variations = modifiers["modified"].unique()
+
+#%% --- Export data ---
+
+#%% --- Export cleaning documentation ---
 
 
