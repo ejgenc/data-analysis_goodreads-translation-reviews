4ad3908bdb31f19ca608ee6bde6b393d3f0d8405
ejgenc <33980760+ejgenc@users.noreply.github.com>
2021-03-11 22:33:41 +0300

    Initialized test_modifiers_cleaned_data_quality.py

---
+++ tests/data_quality_tests/test_modifiers_cleaned_data_quality.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 21:20:24 2021
+
+@author: ejgen
+
+------ What is this file? ------
+
+This test module contains some data quality tests for the
+tokens_and_dependencies_cleaned.csv file.
+
+The file can be found at:
+    ../../data/cleaned/tokens_and_dependencies_cleaned.csv
+    
+"""
+
