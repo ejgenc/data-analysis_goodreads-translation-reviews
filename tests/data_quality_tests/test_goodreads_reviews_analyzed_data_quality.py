3304cd315ec81a86f6f726b864042576caa03edf
ejgenc <33980760+ejgenc@users.noreply.github.com>
2021-03-17 13:56:40 +0300

    Initialized test_goodreads_reviews_analyzed_data_quality.py

---
+++ tests/data_quality_tests/test_goodreads_reviews_analyzed_data_quality.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 13:54:18 2021
+
+@author: ejgen
+
+This test module contains some data quality tests for the
+goodreads_reviews_analyzed.csv file. the file can be found at:
+    ../../data/analysis_results/goodreads_reviews_analyzed.csv
+    
+"""
+
+#%% --- Import required packages ---
+
+import os
+
+from pathlib import Path # To wrap around filepaths
+import pytest
+import pandas as pd
+
+#%% --- Set proper directory to assure integration with doit ---
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
